e7c8e5ae168cbac28a4ec2b74f2dcaab5b48db82
Fantin Bibas <fantin@bib.as>
2021-01-09 22:35:22 +0100

    Simplify

--- src/app/services/midi.service.ts
+++ src/app/services/midi.service.ts
@@ -4,6 +4,24 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MidiService {
+  colors = [
+    {r: 0, g: 0, b: 0, a: 0, code: 0},
+    {r: 255, g: 0, b: 0, a: .3, code: 1},
+    {r: 255, g: 0, b: 0, a: .6, code: 2},
+    {r: 255, g: 0, b: 0, a: 1, code: 3},
+    {r: 0, g: 255, b: 0, a: .3, code: 16},
+    {r: 255, g: 128, b: 0, a: .3, code: 17},
+    {r: 255, g: 128, b: 0, a: .6, code: 18},
+    {r: 255, g: 85, b: 0, a: 1, code: 19},
+    {r: 0, g: 255, b: 0, a: .6, code: 32},
+    {r: 128, g: 255, b: 0, a: .6, code: 33},
+    {r: 255, g: 255, b: 0, a: .6, code: 34},
+    {r: 255, g: 128, b: 0, a: 1, code: 35},
+    {r: 0, g: 255, b: 0, a: 1, code: 48},
+    {r: 85, g: 255, b: 0, a: 1, code: 49},
+    {r: 128, g: 255, b: 0, a: 1, code: 50},
+    {r: 255, g: 255, b: 0, a: 1, code: 51}
+  ];
   errorString?: string;
   // @ts-ignore
   inputs: WebMidi.MIDIInput[] = [];
@@ -62,5 +80,8 @@ export class MidiService {
     this.outputs
       .filter(device => device.id === id)
       .forEach(device => device.send(data));
+  }
+
+  writeColorToPosition(x: number, y: number, color: number): void {
   }
 }
